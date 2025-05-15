237d9692446929c2e94d36861461c1e17a5331de
SCUBE <148875941+scuberanni@users.noreply.github.com>
2025-05-15 10:36:20 +0530

    new

--- manager/views.py
+++ manager/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 
 def home(request):
     return render(request, 'home.html')
 
 @login_required
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    if request.user.username == 'daffodils':
+        return render(request, 'dashboard.html')
+    else:
+        return HttpResponseForbidden("Access denied: You are not authorized to view this page.")
