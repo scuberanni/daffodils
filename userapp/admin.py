de9209b0a8fbf9ab8fdba700981f0ae691f7a9c9
SCUBE <148875941+scuberanni@users.noreply.github.com>
2025-06-09 17:23:43 +0530

    admin

--- userapp/admin.py
+++ userapp/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import UserProfile, TeacherProfile, events, EventPhoto, EventVideo
 
-admin.site.register(UserProfile)
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('get_first_name', 'user', 'phone', 'batch', 'is_approved')
+
+    def get_first_name(self, obj):
+        return obj.user.first_name
+    get_first_name.short_description = 'First Name'
 admin.site.register(TeacherProfile)
 admin.site.register(events)
 admin.site.register(EventPhoto)
--- userapp/models.py
+++ userapp/models.py
@@ -8,15 +8,18 @@ from django.utils import timezone
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=15, unique=True ,default='+91')
+    phone = models.CharField(max_length=15, unique=True, default='+91')
     batch = models.CharField(max_length=20, choices=[
         ('10A', '10A'), ('10B', '10B'), ('10C', '10C'), ('10D', '10D')
     ])
     n_adult = models.PositiveIntegerField(null=True, blank=True, default=0)
-    n_child = models.PositiveIntegerField(null=True, blank=True , default=0)
+    n_child = models.PositiveIntegerField(null=True, blank=True, default=0)
     photo = models.ImageField(upload_to='user_photos/', blank=True, null=True)
 
     is_approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
 
 class TeacherProfile(models.Model):
     name = models.CharField(max_length=20, unique=True)
